6666b4c65f1d446b05374ab618d493fbd6abacf1
aliasadch <aliasadch987@gmail.com>
2026-04-30 23:14:40 +0500

    Update diffusion_edit.py

--- diffusion_edit.py
+++ diffusion_edit.py
@@ -1,23 +1,37 @@
 import torch
-from diffusers import StableDiffusionXLImg2ImgPipeline
+import cv2
+import numpy as np
+from diffusers import StableDiffusionXLInpaintPipeline
 from PIL import Image
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cuda"
 
-pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
+pipe = StableDiffusionXLInpaintPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0",
-    torch_dtype=torch.float16 if device == "cuda" else torch.float32
-)
-pipe = pipe.to(device)
+    torch_dtype=torch.float16
+).to(device)
 
-def edit_image(image_array, prompt, strength=0.4):
-    image = Image.fromarray(image_array)
+pipe.enable_model_cpu_offload()
+
+def edit_image(original_bgr, prompt, face_bbox, strength=0.6):
+
+    image = cv2.cvtColor(original_bgr, cv2.COLOR_BGR2RGB)
+    image_pil = Image.fromarray(image)
+
+    # Create mask
+    mask = np.zeros(image.shape[:2], dtype=np.uint8)
+
+    x1, y1, x2, y2 = map(int, face_bbox)
+    mask[y1:y2, x1:x2] = 255
+
+    mask_pil = Image.fromarray(mask)
 
     result = pipe(
         prompt=prompt,
-        image=image,
-        strength=strength,
-        guidance_scale=7.5
+        image=image_pil,
+        mask_image=mask_pil,
+        guidance_scale=7.5,
+        num_inference_steps=30,
     ).images[0]
 
     return result
